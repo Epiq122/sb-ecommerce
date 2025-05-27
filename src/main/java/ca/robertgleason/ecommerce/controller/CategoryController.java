2c82674b462a0c4993cd9ca60974877d31444ce1
Robert Gleason <l2ob122@gmail.com>
2025-05-27 15:37:41 -0700

    Add CategoryController and Category model
    Introduce `CategoryController` with endpoints to fetch all categories and test the API. Added `Category` model class to represent category data with basic fields and getters/setters.

---
+++ src/main/java/ca/robertgleason/ecommerce/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package ca.robertgleason.ecommerce.controller;
+
+
+import ca.robertgleason.ecommerce.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private final List<Category> categories = new ArrayList<>();
+
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello";
+    }
+
+
+}
