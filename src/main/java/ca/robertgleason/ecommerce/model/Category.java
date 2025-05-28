29876a72d9a24db2278d965e70b0f40e44a4070e
Robert Gleason <l2ob122@gmail.com>
2025-05-27 21:24:53 -0700

    Add JPA annotations to Category entity
    Marked the Category class as a JPA entity using the @Entity annotation and defined the table name as "categories". Added @Id to the primary key field and included a default constructor to support JPA requirements. These changes enable Category to be persisted in the database.

--- src/main/java/ca/robertgleason/ecommerce/model/Category.java
+++ src/main/java/ca/robertgleason/ecommerce/model/Category.java
@@ -1,8 +1,14 @@
 package ca.robertgleason.ecommerce.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")
 public class Category {
 
 
+    @Id
     private Long categoryId;
     private String categoryName;
 
@@ -11,6 +17,11 @@ public class Category {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
     }
+
+    public Category() {
+
+    }
+
 
     public Long getCategoryId() {
         return categoryId;
