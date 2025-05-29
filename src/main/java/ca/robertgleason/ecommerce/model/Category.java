92c2d64914a058a86d9bc8b0f708ebb338482bbb
Robert Gleason <l2ob122@gmail.com>
2025-05-28 18:24:04 -0700

    Add ID generation strategy and enable SQL logging
    Configured `@GeneratedValue` with `GenerationType.IDENTITY` for the `categoryId` field in the `Category` entity to auto-generate IDs. Enabled SQL logging and query formatting in `application.properties` to improve debugging and query visualization.

--- src/main/java/ca/robertgleason/ecommerce/model/Category.java
+++ src/main/java/ca/robertgleason/ecommerce/model/Category.java
@@ -2,6 +2,8 @@ package ca.robertgleason.ecommerce.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
@@ -9,6 +11,7 @@ public class Category {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
