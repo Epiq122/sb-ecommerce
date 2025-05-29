441978bf99c24785af3b21276046e67b1f2871f7
Robert Gleason <l2ob122@gmail.com>
2025-05-28 18:42:47 -0700

    Add CategoryRepository interface for database operations
    Introduce a JpaRepository for the Category entity to manage database interactions. This simplifies CRUD operations and provides a foundation for working with category data.

--- src/main/java/ca/robertgleason/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/ca/robertgleason/ecommerce/service/CategoryServiceImpl.java
@@ -1,36 +1,42 @@
 package ca.robertgleason.ecommerce.service;
 
 import ca.robertgleason.ecommerce.model.Category;
+import ca.robertgleason.ecommerce.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
+    //    private final List<Category> categories = new ArrayList<>();
 
+
+    private CategoryRepository categoryRepository;
+
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
 
     @Override
     public void updateCategory(Category category) {
+        List<Category> categories = categoryRepository.findAll();
         Category existingCategory = categories.stream()
                 .filter(c -> c.getCategoryId().equals(category.getCategoryId()))
                 .findFirst()
@@ -38,17 +44,20 @@ public class CategoryServiceImpl implements CategoryService {
                         "Category with id " + category.getCategoryId() + " not found"));
 
         existingCategory.setCategoryName(category.getCategoryName());
+        categoryRepository.save(existingCategory);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
+
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "requested category not found"));
 
         if (category != null) {
-            categories.remove(category);
+            categoryRepository.delete(category);
             return "Category with id " + categoryId + " deleted";
         }
 
